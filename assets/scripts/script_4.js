d11049822a145ba6248154376718e4be485fc4f9
leuteris alexiou <leualexiou@hotmail.com>
2018-12-08 12:34:46 +0200

    Selection diagram complete

--- assets/scripts/script_4.js
+++ assets/scripts/script_4.js
@@ -1,18 +1,22 @@
+// Store the chart elements to variables
 var pie_chart = document.getElementById("pie-chart");
 var s_chart = document.getElementById("chart");
 var stacked_bar_chart = document.getElementById("stacked-bar-chart");
 
+// The buttons to set the event listeners
 var national = document.getElementsByClassName("national");
 var regional = document.getElementsByClassName("regional");
 var state = document.getElementsByClassName("state");
 
+// Store subtitles and radio-buttons of the charts to variables
 var national_item = document.getElementsByClassName("national_item");
 var regional_item = document.getElementsByClassName("regional_item");
 var state_item = document.getElementsByClassName("state_item");
 
-var selected = 0;
+var selected = 0; // the page start preselected with the pie chart
 MenuSelect();
 
+// event listeners to change the chart when a menu button is pressed
 for(var i = 0; i < national.length; i++){
     national[i].addEventListener('click', function (event) {
         selected = 0;
@@ -34,6 +38,7 @@ for(var i = 0; i < state.length; i++){
       });
 }
 
+// this function change the visibility of the charts and their titles
 function MenuSelect(){
     switch(selected){
         case 0:
